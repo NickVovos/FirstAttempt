a036cddd9a2451184eb6e8502dfc051526c8c864
nikolaos.vovos <nikolaos.vovos@prometric.com>
2024-12-08 23:20:49 +0000

    Mange is perfect. The categories are hardcoded

--- FirstAttempt/Pages/Manage.cshtml.cs
+++ FirstAttempt/Pages/Manage.cshtml.cs
@@ -30,20 +30,248 @@ namespace FirstAttempt.Pages
         }
 
         public List<Recipe> Recipes { get; set; } = new List<Recipe>
-    {
-        new Recipe
         {
-            Id = 1,
-            Title = "Spaghetti Carbonara",
-            Category = "Main",
-            Description = "A classic Italian pasta dish.",
-            Steps = new List<Step>
+            new Recipe
             {
-                new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
-                new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                Id = 1,
+                Title = "Spaghetti Carbonara",
+                Category = "Main",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+             new Recipe
+            {
+                Id = 2,
+                Title = "Nikos",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 3,
+                Title = "Stella",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 4,
+                Title = "Giorgos",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 5,
+                Title = "Amalia",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+                        new Recipe
+            {
+                Id = 6,
+                Title = "Spaghetti Carbonara",
+                Category = "Main",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+             new Recipe
+            {
+                Id = 7,
+                Title = "Nikos",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 8,
+                Title = "Stella",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 9,
+                Title = "Giorgos",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 10,
+                Title = "Amalia",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+                        new Recipe
+            {
+                Id = 11,
+                Title = "Spaghetti Carbonara",
+                Category = "Main",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+             new Recipe
+            {
+                Id = 12,
+                Title = "Nikos",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 13,
+                Title = "Stella",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 14,
+                Title = "Giorgos",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 15,
+                Title = "Amalia",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+                        new Recipe
+            {
+                Id = 16,
+                Title = "Spaghetti Carbonara",
+                Category = "Main",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+             new Recipe
+            {
+                Id = 17,
+                Title = "Nikos",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 18,
+                Title = "Stella",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 19,
+                Title = "Giorgos",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
+            },
+            new Recipe
+            {
+                Id = 20,
+                Title = "Amalia",
+                Category = "Dessert",
+                Description = "A classic Italian pasta dish.",
+                Steps = new List<Step>
+                {
+                    new Step { Id = 1, Title = "Boil Pasta", Description = "Cook the spaghetti in salted water.", Duration = 10, Images = null, Ingredients = null },
+                    new Step { Id = 2, Title = "Prepare Sauce", Description = "Mix eggs and Parmesan cheese.", Duration = 5, Images = null, Ingredients = null }
+                }
             }
-        }
-    };
+        };
 
         public List<Recipe> FilteredRecipes => Recipes
             .Where(r => (string.IsNullOrWhiteSpace(SearchTerm) || r.Title.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase))
@@ -67,9 +295,9 @@ namespace FirstAttempt.Pages
             switch (action)
             {
                 case "View":
-                    return RedirectToPage("/execute", new { id = RecipeId });
+                    return RedirectToPage("/execute", new { recipeId = RecipeId });
                 case "Modify":
-                    return RedirectToPage("/EditRecipe", new { id = RecipeId });
+                    return RedirectToPage("/EditRecipe", new { recipeId = RecipeId });
                 case "Delete":
                     RecipeToDelete = Recipes.FirstOrDefault(r => r.Id == RecipeId);
                     break;
